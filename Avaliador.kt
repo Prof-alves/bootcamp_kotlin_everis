0ca1d46afada3f9cbe4654cacfe8756512e4db6c
unknown <prof.alves03@gmail.com>
2021-03-07 19:55:10 -0300

    Avaliador de notas. Aprendi "random" e "when"

---
+++ Avaliador.kt
@@ -0,0 +1,14 @@
+fun Int.resultado(): String{
+    println("Você tirou $this")
+    return when (this){
+        in 0..6-> "Se deu mal, reprovou"
+        in 7..8-> "Passou"
+        in 9..10-> "Nerd"
+        else-> "O professor ainda não corrigiu a prova"
+    }
+}
+
+fun main(){
+    val notas = (0..10).random()
+    println(notas.resultado())
+}
